364190ca4ad19a597033e9eb6afd655059f73848
Gitea <gitea@fake.local>
2020-05-05 12:53:51 +0300

    Finished Lab 11

---
+++ Asp_Mendela-Alexandru-Abel_Rp/Data/Asp_Mendela_Alexandru_Abel_RpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Asp_Mendela_Alexandru_Abel_Rp.Models;
+
+namespace Asp_Mendela_Alexandru_Abel_Rp.Data
+{
+    public class Asp_Mendela_Alexandru_Abel_RpContext : DbContext
+    {
+        public Asp_Mendela_Alexandru_Abel_RpContext (DbContextOptions<Asp_Mendela_Alexandru_Abel_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asp_Mendela_Alexandru_Abel_Rp.Models.Movie> Movie { get; set; }
+    }
+}
